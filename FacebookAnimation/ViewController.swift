91635b7466f2f91fcdeb4242c04405d4c35d9433
Nikita Teslyuk <teslyuk.nikita@gmail.com>
2019-04-18 15:26:59 +0300

    Initial Commit

---
+++ FacebookAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookAnimation
+//
+//  Created by Nikita Teslyuk on 18/04/2019.
+//  Copyright © 2019 Tesnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
